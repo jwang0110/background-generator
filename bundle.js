e755a0a8c004817ddaf842caeb80e09db8983103
jwang0110 <johnw7@uci.edu>
2021-02-04 16:39:33 -0800

    add git file

--- bundle.js
+++ bundle.js
@@ -1,3 +1,5 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+(function (global){(function (){
 /**
  * @license
  * Lodash <https://lodash.com/>
@@ -17159,3 +17161,44 @@
     root._ = _;
   }
 }.call(this));
+
+}).call(this)}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{}],2:[function(require,module,exports){
+var _ = require('lodash');
+console.log(_);
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var random = document.querySelector("button");
+var body = document.getElementById("gradient");
+
+function generateRandomColor() {
+	var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+	return randomColor;
+}
+
+function setGradient(c1, c2) {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ c1 
+	+ ", " 
+	+ c2 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function inputGradient() {
+	setGradient(color1.value, color2.value);
+}
+
+function randomGradient() {
+	setGradient(generateRandomColor(), generateRandomColor());
+}
+
+inputGradient();
+color1.addEventListener("input", inputGradient);
+color2.addEventListener("input", inputGradient);
+random.addEventListener("click", randomGradient);
+
+},{"lodash":1}]},{},[2]);
